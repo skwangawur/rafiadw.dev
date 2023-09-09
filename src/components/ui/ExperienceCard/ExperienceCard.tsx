67a643001a8456195be0fd5273df5904ae0982b5
skwangawur <rafiadw@gmail.com>
2023-09-09 20:29:22 +0700

    setup project web portofolio

---
+++ src/components/ui/ExperienceCard/ExperienceCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExperienceCard() {
+  return <div>ExperienceCard</div>;
+}
+
+export default ExperienceCard;
---
+++ src/components/ui/ProfilePhoto/ProfilePhoto.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProfilePhoto() {
+  return <div>ProfilePhoto</div>;
+}
+
+export default ProfilePhoto;
