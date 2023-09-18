976df87c72e6dea27220d039ca63ca151252a017
skwangawur <rafiadw@gmail.com>
2023-09-18 14:19:23 +0700

    feat(components): fix styling profilePhoto

--- src/components/ui/ProfilePhoto/ProfilePhoto.tsx
+++ src/components/ui/ProfilePhoto/ProfilePhoto.tsx
@@ -10,7 +10,7 @@ function ProfilePhoto(props: ProfilePhotoProps) {
   return (
     <Link to="/" className="w-24 h-24 rounded-full overflow-hidden mt-10 ">
       <img
-        className="w-full h-full  h-auto hover:scale-110 transition"
+        className="w-full h-full hover:scale-110 transition"
         src={url}
         alt={alt}
       />
