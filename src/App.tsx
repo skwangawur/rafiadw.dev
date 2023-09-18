7383ce6e25bc50fcefbee775aa2ddf184e8bb4b6
skwangawur <rafiadw@gmail.com>
2023-09-18 14:20:03 +0700

    feat: add routing, styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import "./App.css";
-import myImage from "./assets/images/photo-profile.png";
-import AboutMe from "./components/ui/AboutMe";
-import NavBar from "./components/ui/NavBar";
-import ProfilePhoto from "./components/ui/ProfilePhoto";
+import RoutesLink from "./routes/Routes";
 
 function App() {
   return (
@@ -19,16 +16,7 @@ function App() {
         />
         <link rel="canonical" href="https://www.contoh.com/profil-saya" />
       </Helmet>
-      <div className="mx-[5%] lg:grid lg:grid-cols-layout lg:mx-[15%]">
-        <div className="flex justify-center flex-col items-center lg:items-start  ">
-          <ProfilePhoto url={myImage} alt="profile photo rafi aditya" />
-          <AboutMe
-            title="Rafi Aditya"
-            about="Lulusan ITTelkom Surabaya jurusan Teknologi Informasi yang tertarik dalam dunia pemrograman, jaringan dan sistem tertanam. "
-          />
-        </div>
-        <NavBar />
-      </div>
+      <RoutesLink />
     </HelmetProvider>
   );
 }
