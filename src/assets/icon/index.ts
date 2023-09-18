b0d4de1343eba75f9dd81fe67cfa2026219a3900
skwangawur <rafiadw@gmail.com>
2023-09-18 14:13:40 +0700

    feat: add several asset (gifs, icons, images)

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,43 @@
+import {
+  SiTypescript,
+  SiLaravel,
+  SiTailwindcss,
+  SiArduino,
+  SiCplusplus,
+  SiCss3,
+  SiExpress,
+  SiMongodb,
+  SiReact,
+  SiSass,
+  SiJavascript,
+  SiHtml5,
+} from "react-icons/si";
+import { IoLogoNodejs } from "react-icons/io5";
+import {
+  BiDownload,
+  BiCodeAlt,
+  BiLogoGoLang,
+  BiLogoPostgresql,
+} from "react-icons/bi";
+import { TbBrandNextjs } from "react-icons/tb";
+
+export {
+  BiDownload,
+  BiCodeAlt,
+  BiLogoGoLang,
+  BiLogoPostgresql,
+  SiTypescript,
+  SiLaravel,
+  SiTailwindcss,
+  SiArduino,
+  SiCplusplus,
+  IoLogoNodejs,
+  TbBrandNextjs,
+  SiCss3,
+  SiExpress,
+  SiMongodb,
+  SiReact,
+  SiSass,
+  SiJavascript,
+  SiHtml5,
+};
