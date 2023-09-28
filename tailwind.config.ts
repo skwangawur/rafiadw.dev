45e4f818d45564602bd72a075ac650af8954690b
skwangawur <rafiadw@gmail.com>
2023-09-28 21:43:00 +0700

    fix: change the configuration of eslint, tailwind, vite and remove packages dont used at package.json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,40 @@
 module.exports = {
-  darkMode: "class",
-  content: ["./src/**/*.{html,js,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "charcoal-gray": "#161b22",
-        "state-gray": "#7d8590",
-        coral: "#f78166",
-        "dark-slate-gray": "#2f363f",
-        "dark-charcoal-gray": "#21262d",
-        Navbar: "rgba(33, 38, 45, 0.6)",
-      },
-      gridTemplateColumns: {
-        layout: "250px 2fr",
-        "card-lg": "repeat(2 , minmax(0, 260px))",
-        "card-2xl": "repeat(2 , minmax(0, 350px))",
-      },
-      Keyframes: {
-        hired: {
-          "0%, 100%": { transform: "scale(1)" },
-          "50%": { transform: "scale(2)" },
+    darkMode: "class",
+    content: ["./src/**/*.{html,js,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "charcoal-gray": "#161b22",
+                "state-gray": "#7d8590",
+                coral: "#f78166",
+                "dark-slate-gray": "#2f363f",
+                "dark-charcoal-gray": "#21262d",
+                Navbar: "rgba(33, 38, 45, 0.6)",
+            },
+            gridTemplateColumns: {
+                layout: "250px 2fr",
+                "card-lg": "repeat(2 , minmax(0, 260px))",
+                "card-2xl": "repeat(2 , minmax(0, 350px))",
+            },
+            Keyframes: {
+                hired: {
+                    "0%, 100%": { transform: "scale(1)" },
+                    "50%": { transform: "scale(2)" },
+                },
+            },
+            animation: {
+                wave: "wave 2s linear infinite",
+                hired: "hired 2s ease-out infinite",
+            },
         },
-      },
-      animation: {
-        wave: "wave 2s linear infinite",
-        hired: "hired 2s ease-out infinite",
-      },
+        fontFamily: {
+            poppins: ["Poppins", "sans-serif"],
+            montserrat: ["Montserrat", "sans-serif"],
+        },
+        backgroundImage: {
+            night: "url('/src/assets/gif/night.gif')",
+            sky: "url('src/assets/gif/sky.gif')",
+        },
     },
-    fontFamily: {
-      poppins: ["Poppins", "sans-serif"],
-      montserrat: ["Montserrat", "sans-serif"],
-    },
-    backgroundImage: {
-      night: "url('/src/assets/gif/night.gif')",
-      sky: "url('src/assets/gif/sky.gif')",
-    },
-  },
-  plugins: [],
+    plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import rehypeSlug from "rehype-slug";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import mdx from "@mdx-js/rollup";
+import { visit } from "unist-util-visit";
 
-// https://vitejs.dev/config/
+function rehypeMetaAsAttributes() {
+    return (tree) => {
+        visit(tree, "element", (node) => {
+            if (node.tagName === "code" && node.data && node.data.meta) {
+                node.properties.meta = node.data.meta;
+            }
+        });
+    };
+}
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        {
+            enforce: "pre",
+            ...mdx({
+                rehypePlugins: [rehypeMetaAsAttributes, rehypeSlug],
+                providerImportSource: "@mdx-js/react",
+            }),
+        },
+        react(),
+    ],
+    envDir: "./",
+    resolve: {
+        alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+    },
+});
