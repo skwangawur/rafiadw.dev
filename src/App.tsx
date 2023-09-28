cba025ab1c9637d394558e228ad6a3d5fd48e1b2
skwangawur <rafiadw@gmail.com>
2023-09-28 21:41:00 +0700

    feat: add routes, and browser router

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import "./App.css";
 import RoutesLink from "./routes/Routes";
 
 function App() {
-  return (
-    <HelmetProvider>
-      <Helmet>
-        <meta
-          name="description"
-          content="Ini adalah profil saya. Saya memiliki pengalaman dalam pengembangan web dan telah menyelesaikan berbagai proyek."
-        />
-        <meta
-          name="keywords"
-          content="profil, pengalaman, proyek, web development"
-        />
-        <link rel="canonical" href="https://www.contoh.com/profil-saya" />
-      </Helmet>
-      <RoutesLink />
-    </HelmetProvider>
-  );
+    return (
+        <HelmetProvider>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Ini adalah profil saya. Saya memiliki pengalaman dalam pengembangan web dan telah menyelesaikan berbagai proyek."
+                />
+                <meta
+                    name="keywords"
+                    content="profil, pengalaman, proyek, web development"
+                />
+                <link
+                    rel="canonical"
+                    href="https://www.contoh.com/profil-saya"
+                />
+            </Helmet>
+            <RoutesLink />
+        </HelmetProvider>
+    );
 }
 
 export default App;
