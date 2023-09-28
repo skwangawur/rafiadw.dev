23b6608ee387413548f26b3120e0a0f8bd24ffcd
skwangawur <rafiadw@gmail.com>
2023-09-28 21:39:20 +0700

    feat: add routing for blogs, project, and experiences

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,20 +1,25 @@
-import * as React from "react";
 import { Routes, Route } from "react-router-dom";
-import Blogs from "../pages/blogs";
-import Projects from "../pages/projects";
-import Experiences from "../pages/experiences";
-import Layout from "../pages/Layout";
+import * as React from "react";
+import DetailBlog from "@/pages/blogs/DetailBlog/DetailBlog";
+import NotFound from "@/pages/notfound/NotFound";
+
+const LayoutLazy = React.lazy(() => import("@/pages/Layout"));
+const BlogsLazy = React.lazy(() => import("@/pages/blogs"));
+const ProjectstLazy = React.lazy(() => import("@/pages/projects"));
+const ExperiencesLazy = React.lazy(() => import("@/pages/experiences"));
 
 function RoutesLink() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Blogs />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="experiences" element={<Experiences />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<LayoutLazy />}>
+                <Route index element={<BlogsLazy />} />
+                <Route path="projects" element={<ProjectstLazy />} />
+                <Route path="experiences" element={<ExperiencesLazy />} />
+            </Route>
+            <Route path="/blogs/:id" index element={<DetailBlog />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
 }
 
 export default RoutesLink;
